5755a24f43930f38e1b93321bd89af72e3d94117
Matthew Nelson <05nelsonm@protonmail.com>
2019-08-15 10:57:12 -0400

    designed layout for detail activity and connected with list activity

--- app/src/main/java/sats/stackemhigh/wireframeconversion/activity/NewsListActivity.kt
+++ app/src/main/java/sats/stackemhigh/wireframeconversion/activity/NewsListActivity.kt
@@ -1,7 +1,9 @@
 package sats.stackemhigh.wireframeconversion.activity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_news_list.*
 import sats.stackemhigh.wireframeconversion.R
 
 class NewsListActivity : AppCompatActivity() {
@@ -9,5 +11,50 @@ class NewsListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news_list)
+
+        iv_1.setOnClickListener {
+            val detailView = Intent(this, NewsDetailActivity::class.java)
+            startActivity(detailView)
+        }
+
+        iv_2.setOnClickListener {
+            val detailView = Intent(this, NewsDetailActivity::class.java)
+            startActivity(detailView)
+        }
+
+        iv_3.setOnClickListener {
+            val detailView = Intent(this, NewsDetailActivity::class.java)
+            startActivity(detailView)
+        }
+
+        iv_4.setOnClickListener {
+            val detailView = Intent(this, NewsDetailActivity::class.java)
+            startActivity(detailView)
+        }
+
+        iv_5.setOnClickListener {
+            val detailView = Intent(this, NewsDetailActivity::class.java)
+            startActivity(detailView)
+        }
+
+        iv_6.setOnClickListener {
+            val detailView = Intent(this, NewsDetailActivity::class.java)
+            startActivity(detailView)
+        }
+
+        iv_7.setOnClickListener {
+            val detailView = Intent(this, NewsDetailActivity::class.java)
+            startActivity(detailView)
+        }
+
+        iv_8.setOnClickListener {
+            val detailView = Intent(this, NewsDetailActivity::class.java)
+            startActivity(detailView)
+        }
+
+        iv_9.setOnClickListener {
+            val detailView = Intent(this, NewsDetailActivity::class.java)
+            startActivity(detailView)
+        }
     }
 }
